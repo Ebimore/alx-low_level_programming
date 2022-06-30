c56421723fa9dff50e4fea3ed590c2cd2ba8f722
Ebimore <ebimoreclaudius@gmail.com>
2022-06-30 15:54:18 -0700

    Updating tasks 4-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main: prints the input message
+ * return: 0 if exited
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n";
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main: prints input
+ * return: 0 if exited
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
