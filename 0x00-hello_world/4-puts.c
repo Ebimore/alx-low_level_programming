9ab969edf47fab35f4141070de9266f4a6b62055
Ebimore <ebimoreclaudius@gmail.com>
2022-06-30 19:34:30 -0700

    Fixing errors in 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the input message
- * return: 0 if exited
- */
+* main - prints the input message
+* Return: 0 if exited
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n";
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
- * main - prints input
- * return: 0 if exited
- */
+/**
+* main - prints input
+* Return: 0 if exited
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
