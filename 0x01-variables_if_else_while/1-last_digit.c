b77c055061b012332f2b59c863fbc286fd91ec33
Ebimore <ebimoreclaudius@gmail.com>
2022-07-02 09:35:54 -0700

    Fixing task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 		printf("and is greater than 5");
 	if (n == 0)
 		printf("and is 0");
-	if (n < 6 && != 0)
+	if (n < 6 && n != 0)
 		printf("and is less than 6 and not 0");
 	printf("\n");
 	return (0);
