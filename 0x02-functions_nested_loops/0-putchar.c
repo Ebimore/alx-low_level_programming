ff0a87dd0194e1cfbda2e6ff97cc69d6dd10d250
Ebimore <ebimoreclaudius@gmail.com>
2022-07-11 07:51:56 -0700

    Testing task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints the string putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char z = "_putchar"
+
+	printf("%s\n", z);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains lessons and tasks on C functions and nested loops
