ad6145b401da158c3f61bb76740c0c7e5d50901d
Ebimore <ebimoreclaudius@gmail.com>
2022-07-16 19:53:14 -0700

    Updating tasks 1 and 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets 10x
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+			_putchar(y);
+	}
+	_putchar('\n');
+}
