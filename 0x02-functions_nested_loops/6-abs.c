1a925c221ff18b8d4efdf367aeef27930989a985
Ebimore <ebimoreclaudius@gmail.com>
2022-07-24 06:10:50 -0700

    Updating tasks 5,6,7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of an integer.
+ * @n: argument
+ * Return: absolute value.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
