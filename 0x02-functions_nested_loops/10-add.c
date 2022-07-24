25e622912ba8f03adb55c4df662e99b101fbc9ee
Ebimore <ebimoreclaudius@gmail.com>
2022-07-24 09:36:29 -0700

    Updating 8 to 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @x: first argument
+ * @y: second argument
+ * Return: Result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
