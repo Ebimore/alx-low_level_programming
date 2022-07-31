46faca524924b2cadcc983273f48354733cf4c47
Ebimore <ebimoreclaudius@gmail.com>
2022-07-31 07:14:11 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks if a character is uppercase
+ * @c:argument
+ * Return: something
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains further tasks on C functions and nested loops.
