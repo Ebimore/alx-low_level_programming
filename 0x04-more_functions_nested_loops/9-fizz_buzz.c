b332c1565eec86975ae97202b7c1e3fd94307995
Ebimore <ebimoreclaudius@gmail.com>
2022-07-31 17:54:34 -0700

    Updating tasks

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - checks code
@@ -6,6 +6,19 @@
  */
 int main(void)
 {
+	int x;
 
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz ");
+		else if (x % 3 == 0)
+			printf("Fizz ");
+		else if (x % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", x);
+	}
+	printf("\n");
 	return (0);
 }
