5df521e591c19e1587209a8d337b22ba18a5da08
Ebimore <ebimoreclaudius@gmail.com>
2022-08-11 17:35:54 -0700

    Updating 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <std.lib>
+#include <std.io>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a linked list.
+ * @head: the pointer to the first node of the linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
