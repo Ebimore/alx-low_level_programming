09524ee465433acb08530f558d65c16b4b5fb362
Ebimore <ebimoreclaudius@gmail.com>
2022-08-16 15:23:38 -0700

    Adding 9,10,100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: the head of a linked list
+ * Return: a pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	if (head)
+	{
+		while (*head)
+		{
+			next = *head;
+			*head = (*head)->next;
+			next->next = prev;
+			prev = next;
+		}
+		*head = prev;
+		return (*head);
+	}
+	return (NULL);
+}
